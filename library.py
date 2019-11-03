d93bb48293158d9a74aec980fe80e04c4614c037
AbishekSeshan <abishek.seshan@outlook.com>
2019-11-03 22:05:18 +0530

    Update code

--- library.py
+++ library.py
@@ -1,11 +1,11 @@
-import cv2 
-import numpy as np 
+import cv2
+import numpy as np
 
 def boundary_adjacent_count(pre_image, boundary_vector):
 	'''Counts the number of adjacent pixel changes in a particular boundary'''
 
 	[m,m] = pre_image.shape
-	
+
 	for count in range(int(m/2)):
 
 		#top:
@@ -20,8 +20,8 @@ def boundary_adjacent_count(pre_image, boundary_vector):
 
 		#bottom:
 		for i in range(m-count-1,count,-1):
-			if pre_image[m-count-1][i] != pre_image[m-count-1][i-1]:			
-				boundary_vector[count][pre_image[m-count-1][i]][1] += 1 
+			if pre_image[m-count-1][i] != pre_image[m-count-1][i-1]:
+				boundary_vector[count][pre_image[m-count-1][i]][1] += 1
 
 		#left:
 		for j in range(m-count-1,count,-1):
@@ -42,7 +42,7 @@ def boundary_frequency(pre_image):
 
 	for count in range(0,int(m/2),1):
 
-		#top and bottom 
+		#top and bottom
 		for i in range(count,m-count):
 
 			boundary_vector[count][pre_image[count][i]][0] += 1
@@ -52,7 +52,7 @@ def boundary_frequency(pre_image):
 			boundary_vector[count][pre_image[m-count-1][i]][0] += 1
 			#if pre_image[m-count][i] != pre_image[m-count][i-1]:
 				#boundary_vector[pre-image[count][i]][1] += 1
-		
+
 
 		#left and right
 		for j in range(count+1,m-count-1):
@@ -65,33 +65,33 @@ def boundary_frequency(pre_image):
 				#boundary_vector[pre-image[count][i]][1] += 1
 
 	return boundary_vector
-	
+
 
 def preprocess_dim(image):
-    '''Resize the image for suitable comparison'''
-	
+	'''Resize the image for suitable comparison'''
+
 	grayimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #matrix
-	[m,n] = grayimage.shape 
-	
+	[m,n] = grayimage.shape
+
 	if m>n:
 		if m%2 == 0:
 			grayimage = cv2.resize(grayimage,(m,m))
 		else:
-			grayimage = cv2.resize(grayimage,(m+1,m+1)) 
+			grayimage = cv2.resize(grayimage,(m+1,m+1))
 	else:
 		if n%2 == 0:
 			grayimage = cv2.resize(grayimage,(n,n))
 		else:
 			grayimage = cv2.resize(grayimage,(n+1,n+1))
 
-	grayimage = cv2.resize(grayimage,(6,6)) 
-	
+	grayimage = cv2.resize(grayimage,(200,200))
+
 	return grayimage
 
 def get_feature_vector(image):
-		
+
 	'''Get final feature vector for an image'''
-		
+
 	preprocessed_image = preprocess_dim(image)
 	boundary_vector = boundary_frequency(preprocessed_image)
 	final_vector = boundary_adjacent_count(preprocessed_image,boundary_vector)
@@ -100,19 +100,48 @@ def get_feature_vector(image):
 
 def print_vector(feature_vector):
 
-    '''Print the feature vector'''
-    
-    for i in range(3):
-        print("boundary {}".format(i))
-        for j in range(256):
-            print(j,feature_vector[i][j][0],feature_vector[i][j][1])
+	'''Print the feature vector'''
+
+	for i in range(100):
+		print("boundary {}".format(i))
+		for j in range(256):
+			print(j,feature_vector[i][j][0],feature_vector[i][j][1])
 
 def print_non_zero(feature_vector):
 
-    '''Print the non zero elements of the feature vector'''
-    
-    for i in range(3):
-        print("boundary {}".format(i))
-        for j in range(256):
-            if feature_vector[i][j][0] and feature_vector[i][j][1] != 0:
-                print(j,feature_vector[i][j][0],feature_vector[i][j][1])
+	'''Print the non zero elements of the feature vector'''
+
+	for i in range(100):
+		print("boundary {}".format(i))
+		for j in range(256):
+			if feature_vector[i][j][0] and feature_vector[i][j][1] != 0:
+				print(j,feature_vector[i][j][0],feature_vector[i][j][1])
+
+
+def compare(image1, image2):
+	
+	'''Compare the feature vectors of 2 images'''
+
+	fv1 = get_feature_vector(image1)
+	fv2 = get_feature_vector(image2) 
+	
+	distance = 0
+	total_count = 0
+	for i in range(100):
+		boundary_sum = 0
+		freq = 0 
+		for j in range(256):
+			a = fv1[i][j][0] + fv2[i][j][0]
+			b = fv1[i][j][1] + fv2[i][j][1]
+			c = fv1[i][j][0] - fv2[i][j][0]
+			d = fv1[i][j][1] - fv2[i][j][1]
+			if a + b != 0:
+				boundary_sum += abs(c) + abs(d)
+				freq += fv1[i][j][0]
+		
+		total_count += freq
+		distance += boundary_sum*freq 
+
+	distance = float(distance/total_count) 
+
+	return distance 
--- main.py
+++ main.py
@@ -1,37 +1,21 @@
-from library import get_feature_vector, print_vector, print_non_zero
+from library import get_feature_vector, print_vector, print_non_zero, compare
 import cv2
 import numpy as np 
 
 
-def compare(image1, image2):
-    
-    '''Compare the feature vectors of 2 images'''
-
-    fv1 = get_feature_vector(image1)
-    fv2 = get_feature_vector(image2) 
-    
-    distance = 0
-
-    for i in range(3):
-        boundary_sum = 0
-        freq = 0 
-        for j in range(256):
-            freq += fv1[i][j][0] #frequency of ith boundary, jth pixel
-            boundary_sum += abs(fv1[i][j][0] -fv2[i][j][0]) + abs(fv1[i][j][1] -fv2[i][j][1])
-        
-        distance += boundary_sum/freq
-
-    return distance 
-
-
 if __name__ == "__main__":
-    
-    query_image = cv2.imread('query.jpg')
-    db_image1 = cv2.imread('test1.jpg')
-    db_image2 = cv2.imread('test2.jpg')    
-    distance1 = compare(db_image1, query_image)
-    distance2 = compare(db_image2, query_image) 
-    print("Distance 1 is: {}".format(distance1))
-    print("Distance 2 is: {}".format(distance2)) 
-    
-    
+	
+	query_image = cv2.imread('query.jpg')
+	db_image1 = cv2.imread('test1.jpg')
+	db_image2 = cv2.imread('test2.jpg')
+	db_image3 = cv2.imread('test3.jpg')    
+	distance1 = compare(db_image1, query_image)
+	distance2 = compare(db_image2, query_image)
+	distance3 = compare(db_image3, query_image) 
+	print("Distance 1 is: {}".format(distance1))
+	print("Distance 2 is: {}".format(distance2))
+	print("Distance 3 is: {}".format(distance2))
+	print_non_zero(db_image3)
+	print_non_zero(query_image)
+	
+	
